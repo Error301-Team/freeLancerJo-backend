b4810f1ea49d13e0b9982a4a6dab97779574935d
Wasem idres <86596948+WasemIdres@users.noreply.github.com>
2021-09-24 17:50:13 +0300

    Update server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict";
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+const mongoose = require("mongoose");
+require("dotenv").config();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT;
+app.get('/', (req, res) => {
+    res.status(200).json({ "message": "I'm working" })
+})
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+})
