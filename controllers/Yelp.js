7b851683dfa3baeed5284beccd8fa3d70e8b8851
WasemIdres <thewasem7915@gmail.com>
2021-09-24 20:09:56 +0300

    get yelp api added

---
+++ controllers/Yelp.js
@@ -0,0 +1,28 @@
+"use strict";
+const axios = require("axios");
+const businesse = require("../models/Yelp.model");
+const businesseController = async (req, res) => {
+    let location = req.query.location;
+    let url = `https://api.yelp.com/v3/businesses/search?location=${location}`
+      
+    let axiosResponse = await axios.get(url ,{ headers:  {"Authorization" : `Bearer ${process.env.YELP_API_Key}`} });
+    let businesseData = axiosResponse.data.businesses;
+    let result=businesseData.map(Element=>{
+        return new businesse(
+            Element.id,
+            Element.alias,
+            Element.name,
+            Element.image_url,
+            Element.review_count,
+            Element.categories,
+            Element.rating,
+            Element.coordinates,
+            Element.transactions,
+            Element.location,
+            Element.phone,
+            Element.display_phone,
+        )
+    })
+    res.status(200).json(result);
+}
+module.exports = businesseController;
