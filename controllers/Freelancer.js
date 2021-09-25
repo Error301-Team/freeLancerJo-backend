28a31ef5c7014842f199793726ba6f3c2f33b730
WasemIdres <thewasem7915@gmail.com>
2021-09-25 21:34:20 +0300

    search for freelancer

--- controllers/Freelancer.js
+++ controllers/Freelancer.js
@@ -2,7 +2,8 @@
 const axios = require("axios");
 const Employees = require("../models/Freelancer.model");
 const freeLancerController = async (req, res) => {
-    let url = `https://www.freelancer.com/api/projects/0.1/jobs/search`
+    let job_names= req.query.job_names;
+    let url = `https://www.freelancer.com/api/projects/0.1/jobs/search/?job_names%5B%5D=${job_names}`
     let axiosResponse = await axios.get(url);
     let freeLancer = axiosResponse.data.result;
     let result = freeLancer.map(Element => {
