7e7df8ce90b1df46c985d371a621b0a1982fa251
WasemIdres <thewasem7915@gmail.com>
2021-09-26 09:01:37 +0300

    added Create for users

--- controllers/User.js
+++ controllers/User.js
@@ -6,4 +6,23 @@ let get_users = async (req, res) => {
     res.status(201).json(users_list);
 }
 
-module.exports = {get_users};
+let create_user = async (req, res) => {
+    let { name, rating, img, email, phoneNumber, skills, experience, job_describtion, price, location } = req.body;
+    let newUser = new userModel({
+        name : name,
+        rating : rating,
+        img : img,
+        email : email,
+        phoneNumber : phoneNumber,
+        skills : skills,
+        experience : experience,
+        job_describtion : job_describtion,
+        price : price,
+        location : location
+    });
+    newUser.save();
+    let users_list = await userModel.find({});
+    res.status(201).json(users_list);
+}
+
+module.exports = { get_users,create_user };
